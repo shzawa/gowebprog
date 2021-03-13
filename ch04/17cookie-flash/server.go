baefcc4fe36f26a620cb2832526073f2fcdb869f
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-03-13 22:25:48 +0900

    update: コメント追記

--- ch04/17cookie-flash/server.go
+++ ch04/17cookie-flash/server.go
@@ -10,6 +10,8 @@ import (
 
 func setMessage(w http.ResponseWriter, r *http.Request) {
 	msg := []byte("Hello World!")
+
+	// ヘッダー内ではCookieのValueをURLエンコードしておく必要がある
 	c := http.Cookie{
 		Name:  "flash",
 		Value: base64.URLEncoding.EncodeToString(msg),
@@ -20,16 +22,19 @@ func setMessage(w http.ResponseWriter, r *http.Request) {
 func showMessage(w http.ResponseWriter, r *http.Request) {
 	c, err := r.Cookie("flash")
 	if err != nil {
-		if err == http.ErrNoCookie {
+		if err == http.ErrNoCookie { // Cookieの有効期限切れの場合もtrue
 			fmt.Fprintln(w, "メッセージがありません。")
 		}
 	} else {
+		// Cookieの削除処理
 		rc := http.Cookie{
 			Name:    "flash",
 			MaxAge:  -1,
 			Expires: time.Unix(1, 0),
 		}
 		http.SetCookie(w, &rc)
+
+		// 上記で作成したCookieのValueはURLエンコード済な為、デコードした上で扱う
 		val, _ := base64.URLEncoding.DecodeString(c.Value)
 		fmt.Fprintln(w, string(val))
 	}
