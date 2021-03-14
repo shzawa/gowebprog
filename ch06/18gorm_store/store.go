17a11dc5b207ed55fc31ed99df286ad8b94a69b9
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-03-14 21:08:49 +0900

    update: reflectを使ってフィールド名を直接指定せずにDB操作

--- ch06/18gorm_store/store.go
+++ ch06/18gorm_store/store.go
@@ -1,15 +1,17 @@
 // リスト6.18
-/* 
+/*
  1. go get github.com/jinzhu/gorm
  2. go run store.go
 */
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"reflect"
+	"time"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
-	"time"
 )
 
 type Post struct {
@@ -24,7 +26,7 @@ type Comment struct {
 	Id        int
 	Content   string
 	Author    string `sql:"not null"`
-	PostId    int    
+	PostId    int
 	CreatedAt time.Time
 }
 
@@ -42,20 +44,22 @@ func init() {
 
 func main() {
 	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
-  fmt.Println(post) // {0 Hello World! Sau Sheong [] 0001-01-01 00:00:00 +0000 UTC}
-  
+	fmt.Println(post) // {0 Hello World! Sau Sheong [] 0001-01-01 00:00:00 +0000 UTC}
+
 	// Create a post
 	Db.Create(&post)
-  fmt.Println(post) // {1 Hello World! Sau Sheong [] 2015-04-13 11:38:50.91815604 +0800 SGT}
-  
-  // Add a comment
-	comment := Comment{Content: "いい投稿だね！", Author: "Joe"}
-	Db.Model(&post).Association("Comments").Append(comment)
+	fmt.Println(post) // {1 Hello World! Sau Sheong [] 2015-04-13 11:38:50.91815604 +0800 SGT}
 
-  // Get comments from a post
-	var readPost Post  
+	// Add a comment
+	comment := Comment{Content: "いい投稿だね！", Author: "Joe"}
+	// Db.Model(&post).Association("Comments").Append(comment)
+	fieldName := reflect.ValueOf(post).Type().Field(3).Name
+	Db.Model(&post).Association(fieldName).Append(comment)
+
+	// Get comments from a post
+	var readPost Post
 	Db.Where("author = $1", "Sau Sheong").First(&readPost)
-  var comments []Comment
-  Db.Model(&readPost).Related(&comments)
-  fmt.Println(comments[0]) // {1 Good post! Joe 1 2015-04-13 11:38:50.920377 +0800 SGT}
+	var comments []Comment
+	Db.Model(&readPost).Related(&comments)
+	fmt.Println(comments) // {1 いい投稿だね！ Joe 1 2015-04-13 11:38:50.920377 +0800 SGT}
 }
